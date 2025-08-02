742dd7893d9f1b83e8eb517f18d74399d7cfb2a2
CSPercival <cyrek2003@gmail.com>
2025-08-02 20:24:02 +0200

    Add cf argument which generates files for contest

--- clank/main.py
+++ clank/main.py
@@ -4,12 +4,27 @@ def greet():
     print("Hello, my name is Clank.")
     print("What can I do for you?")
 
+def prepare_files(prefix, number_of_problems):
+    print("Preparing files")
+    for i in range(number_of_problems):
+        path = prefix + chr(i + ord('A')) + ".txt"
+
+        with open(path, 'w') as file:
+            file.write("Clank was here")
+            
+        print(f"Created {path} file")
+
 def main():
     parser = argparse.ArgumentParser(description = "Tool for automating competitive programming related tasks")
 
     parser.add_argument("--greet", action="store_true", help="Print greeting message")
+    parser.add_argument("--cf", nargs = 2, type = str, help="Create files for contest")
 
     args = parser.parse_args()
 
     if args.greet:
         greet()
+    
+    if args.cf:
+        prefix, number_of_problems = args.cf
+        prepare_files(prefix, int(number_of_problems))
