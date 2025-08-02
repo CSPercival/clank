be039ccdbafa5970bb4d691a0d5e70ba88581b5c
CSPercival <cyrek2003@gmail.com>
2025-08-02 19:45:16 +0200

    add greet argument

--- clank/main.py
+++ clank/main.py
@@ -1,5 +1,15 @@
+import argparse
+
+def greet():
+    print("Hello, my name is Clank.")
+    print("What can I do for you?")
+
 def main():
-    line = input()
-    while(line != "q" and line != "Q"):
-        print(line)
-        line = input()
+    parser = argparse.ArgumentParser(description = "Tool for automating competitive programming related tasks")
+
+    parser.add_argument("--greet", action="store_true", help="Print greeting message")
+
+    args = parser.parse_args()
+
+    if args.greet:
+        greet()
