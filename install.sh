4885858cffd2fecc947f9849adcac35b8b209d12
CSPercival <cyrek2003@gmail.com>
2025-08-02 16:37:04 +0200

    add first version of installation script
    - write install.sh script
    - rename wrapper for clank from clank.py to run_clank.py
      because of conflict with clank directory

--- install.sh
+++ install.sh
@@ -1,2 +1,19 @@
-# TODO:
-# script that makes clank visible globally
+#!/bin/bash
+
+MAIN_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CLANK="$MAIN_DIR/run_clank.py"
+
+#Permission
+echo "Give permission to clank"
+chmod +x "$CLANK"
+
+#Symlink
+echo "Create symlink"
+sudo ln -sf "$CLANK" /usr/local/bin/clank
+
+#Test
+if command -v clank &> /dev/null; then
+    echo "Installation completed successfully"
+else
+    echo "Installation crashed!"
+fi
