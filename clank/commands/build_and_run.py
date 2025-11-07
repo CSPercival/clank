4f8921919c772f3f1578334f64a302b5cda5028d
Cyryl Szatan <cszatan@LAPTOP-OMEN.localdomain>
2025-11-08 00:19:39 +0100

    Add debug option to the build and run command

--- clank/commands/build_and_run.py
+++ clank/commands/build_and_run.py
@@ -1,7 +1,3 @@
-# import subprocess
-# from pathlib import Path
-# import os
-# import sys
 from clank.commands.build import build
 from clank.commands.run import run
 
@@ -10,8 +6,9 @@ def build_and_run_init_parser(parser):
     subparser.add_argument("file_name", type=str, help="Name of file to compile and run")
     subparser.add_argument("input_file", nargs="?", type=str, help="Name of the input file")
     subparser.add_argument("output_file", nargs="?", type=str, help="Name of the output file")
+    subparser.add_argument("-d", "--debug", action="store_true", help="Enable debug mode")
 
-def build_and_run(file_name, input_file, output_file):
-    if build(file_name) == 0:
+def build_and_run(file_name, input_file, output_file, debug_option_enabled):
+    if build(file_name, debug_option_enabled) == 0:
         run(file_name, input_file, output_file)
     
