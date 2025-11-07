64236f61c733ad707d7703ca753f3b714a3fe0e9
Cyryl Szatan <cszatan@LAPTOP-OMEN.localdomain>
2025-11-07 23:12:33 +0100

    Add second .cpp template
    Add new simpler template for .cpp programs,
    as not all programs need testcases.

---
+++ clank/resources/CPP_template.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define st first
+#define nd second
+#define all(x) x.begin(), x.end()
+#define BOOST cin.tie(NULL); ios_base::sync_with_stdio(false);
+// #define int ll
+typedef long long ll;
+
+ 
+int32_t main(){
+    BOOST;
+    
+    return 0;
+}
